292641079c358ed5e799803a71368a62566223a0
cozej4 <ilakozejumanne@gmail.com>
2019-06-19 10:22:14 +0300

    Code refactoring

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import VisitParcel from "./visitsParcel";
+import VisitParcel from "./visits-parcel";
 import { BrowserRouter } from "react-router-dom";
 
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
