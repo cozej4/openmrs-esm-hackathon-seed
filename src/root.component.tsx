375fbbf8a0f21b1150507d80d5c97c8131196b40
cozej4 <ilakozejumanne@gmail.com>
2019-06-20 14:33:41 +0300

    Updated parcell styles

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -3,9 +3,9 @@ import LatestObs from "./widgets/latest-obs-parcel";
 
 export default function Root(props: RootProps) {
   return (
-    <div>
-      <div className="test">
-        <h2>{"Latest Observations"}</h2>
+    <div className="card">
+      <div className="card-header">
+        <h5 className="card-title">Latest Observations</h5>
       </div>
       <LatestObs patientUuid={props.patientUuid} />
     </div>
